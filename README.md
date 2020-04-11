eda516e0de816cbe73e744a247ae714eac281caa
Carrot7712 <carrot7712@gmail.com>
2020-04-11 21:28:23 +0800

    upload my work

---
+++ README.md
@@ -0,0 +1,7 @@
+# RGB-to-Hex-Converter
+[Try it now!](https://carrot7712.github.io/RGB-to-Hex-Converter/)
+![](https://i.imgur.com/Q0V6HE2.png)
+
+
+A fabulous RGB to Hex converter starting with Pantone Color of the Year 2020, Classical Blue!
+(AC-semester-2-2-final-exam-Q2)
---
+++ main.js
@@ -0,0 +1,49 @@
+(function(){
+    //父層，裝監聽器
+    const colorPicker=document.getElementById('color-picker') 
+    //抓R、G、B顯示數值
+    const redValue=document.getElementById('red-value') 
+    const greenValue=document.getElementById('green-value') 
+    const blueValue=document.getElementById('blue-value')
+    //抓hex code呈現區
+    const hexPanel=document.getElementById('hex-panel') 
+    //背景，取得以視覺化數值
+    const background =document.getElementById('background')
+    //設定預設值
+    let hexCode='#0F4C81'
+    let R_hex='0F',G_hex='4C',B_hex='81'
+
+    //將hexCode顯示並設為背景色
+    hexPanel.innerText=hexCode
+    background.setAttribute('style',`background-color:${hexCode};`)
+    // 以下同效果
+    // background.style.backgroundColor=hexCode
+
+    //=====EventListener=====
+                                //input值一被改變就會觸發input事件
+    colorPicker.addEventListener('input',(event)=>{
+        //1.把input的值打在下一行span上
+        event.target.nextElementSibling.innerText=event.target.value
+        //2.去拿那個span的值
+        //3.字串轉成數字(10進位)
+        //4.10進位數轉成16進位字串       
+        //5.存回變數中
+        //6.例外處理：萬一只有一位，前面補0
+        R_hex=Number(redValue.innerText).toString(16)
+        if(R_hex.length===1){
+            R_hex=`0${R_hex}`
+        }
+        G_hex=Number(greenValue.innerText).toString(16)
+        if(G_hex.length===1){
+            G_hex=`0${G_hex}`
+        }
+        B_hex=Number(blueValue.innerText).toString(16)
+        if(B_hex.length===1){
+            B_hex=`0${B_hex}`
+        }
+        hexCode=`#${R_hex}${G_hex}${B_hex}`
+        //更新顯示、上色
+        hexPanel.innerText=hexCode
+        background.setAttribute('style',`background-color:${hexCode};`)
+    })
+})()
